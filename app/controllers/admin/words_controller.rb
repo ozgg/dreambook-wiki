0ef8900022ca9c269c85feda0cec4b55c9554548
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-03-24 16:24:12 +0300

    Added routes and empty controllers

---
+++ app/controllers/admin/words_controller.rb
@@ -0,0 +1,2 @@
+class Admin::WordsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   root 'index#index'
+
+  resources :patterns, except: [:index, :show]
+  resources :words, except: [:index, :show]
+
+  namespace :admin do
+    resources :patterns, only: [:index, :show] do
+      member do
+        put 'lock', defaults: { format: :json }
+        delete 'lock', action: :unlock, defaults: { format: :json }
+      end
+    end
+    resources :words, only: [:index, :show]
+  end
 end
